17f657f7b573e22284649e916cb7a9afa5161180
Mwaki <mwaki@example.com>
2026-05-23 15:28:00 +0300

    Your message

--- config/database.php
+++ config/database.php
@@ -103,9 +103,7 @@ return [
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'require'),
 
-            'options' => extension_loaded('pdo_pgsql') ? [
-                PDO::PGSQL_ATTR_DISABLE_PREPARES => true,
-            ] : [],
+            'options' => [],
         ],
 
         'sqlsrv' => [
