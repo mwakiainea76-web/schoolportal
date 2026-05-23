6a1305a6077ebfd53d8cfdb761329ab3b3874b93
Mwaki <mwaki@example.com>
2026-05-23 15:26:15 +0300

    Your message

--- config/database.php
+++ config/database.php
@@ -85,28 +85,28 @@ return [
         ],
 
         'pgsql' => [
-    'driver' => 'pgsql',
+            'driver' => 'pgsql',
 
-    // IMPORTANT
-    'url' => env('DB_URL'),
+            // Uses DB_URL from .env
+            'url' => env('DB_URL'),
 
-    'host' => env('DB_HOST', '127.0.0.1'),
-    'port' => env('DB_PORT', '5432'),
-    'database' => env('DB_DATABASE', 'laravel'),
-    'username' => env('DB_USERNAME', 'root'),
-    'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
 
-    'charset' => env('DB_CHARSET', 'utf8'),
-    'prefix' => '',
-    'prefix_indexes' => true,
-    'search_path' => 'public',
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
 
-    'sslmode' => env('DB_SSLMODE', 'require'),
+            'search_path' => 'public',
+            'sslmode' => env('DB_SSLMODE', 'require'),
 
-    'options' => [
-        PDO::PGSQL_ATTR_DISABLE_PREPARES => true,
-    ],
-],
+            'options' => extension_loaded('pdo_pgsql') ? [
+                PDO::PGSQL_ATTR_DISABLE_PREPARES => true,
+            ] : [],
+        ],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
