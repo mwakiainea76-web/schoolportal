468b77e490fd977405c3c56e1ed89929b288435c
Mwaki <mwaki@example.com>
2026-05-23 15:17:47 +0300

    Your message

--- config/database.php
+++ config/database.php
@@ -86,23 +86,26 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
+
+            // IMPORTANT
+            'url' => env('DATABASE_URL'),
+
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
+
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'sslmode' => env('DB_SSLMODE', 'require'),
+
             'options' => array_filter([
                 PDO::PGSQL_ATTR_DISABLE_PREPARES => true,
-                'options' => env('DB_OPTIONS'),
             ]),
         ],
-
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DB_URL'),
