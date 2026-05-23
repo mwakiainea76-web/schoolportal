71e29c920d6c747f5f7c426f354678c55e89eb5b
Mwaki <mwaki@example.com>
2026-05-23 15:23:11 +0300

    Your message

--- config/database.php
+++ config/database.php
@@ -79,33 +79,35 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => [
-                PDO::PGSQL_ATTR_DISABLE_PREPARES => true,
-            ],
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
 
         'pgsql' => [
-            'driver' => 'pgsql',
+    'driver' => 'pgsql',
 
-            // IMPORTANT
-            'url' => env('DB_URL'),
+    // IMPORTANT
+    'url' => env('DB_URL'),
 
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+    'host' => env('DB_HOST', '127.0.0.1'),
+    'port' => env('DB_PORT', '5432'),
+    'database' => env('DB_DATABASE', 'laravel'),
+    'username' => env('DB_USERNAME', 'root'),
+    'password' => env('DB_PASSWORD', ''),
 
-            'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => env('DB_SSLMODE', 'require'),
+    'charset' => env('DB_CHARSET', 'utf8'),
+    'prefix' => '',
+    'prefix_indexes' => true,
+    'search_path' => 'public',
 
-            'options' => array_filter([
-                PDO::PGSQL_ATTR_DISABLE_PREPARES => true,
-            ]),
-        ],
+    'sslmode' => env('DB_SSLMODE', 'require'),
+
+    'options' => [
+        PDO::PGSQL_ATTR_DISABLE_PREPARES => true,
+    ],
+],
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DB_URL'),
