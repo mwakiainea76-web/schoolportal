6023cd89907c6e68e52d2897aa799d8afb668a15
Mwaki <mwaki@example.com>
2026-05-23 15:21:15 +0300

    Your message

--- config/database.php
+++ config/database.php
@@ -79,9 +79,9 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'options' => [
+                PDO::PGSQL_ATTR_DISABLE_PREPARES => true,
+            ],
         ],
 
         'pgsql' => [
